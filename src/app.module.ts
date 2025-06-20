6512d88b932716e4a1ca01489e33a28c08d8bbd2
edith gonzalez <karen.gonzalez4820@gmail.com>
2025-06-20 15:39:30 -0500

    feat: add task module with TypeORM integration and configuration
    - Added @nestjs/config and @nestjs/typeorm dependencies to package.json.
    - Configured global configuration module in app.module.ts to load environment variables.
    - Set up TypeORM with MySQL connection using async configuration in app.module.ts.
    - Created TaskModule with TaskController and TaskService.
    - Implemented basic structure for TaskController and TaskService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,30 @@
+import { Config } from './../node_modules/@sqltools/formatter/lib/core/types.d';
 import { Module } from '@nestjs/common';
+import { TaskModule } from './task/task.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (Config: ConfigService) => ({
+        type: 'mysql',
+        host: Config.get('DB_HOST'),
+        port: Config.get('DB_PORT'),
+        username: Config.get('DB_USER'),
+        // password: Config.get('DB_PASSWORD'),
+        database: Config.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts, .js,}'],
+        synchronize: true,
+      }),
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
